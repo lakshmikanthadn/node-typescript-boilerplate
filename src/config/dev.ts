ea2994b37b8d5dd1d5777d41e55c23e3943e28db
laskhmikanthadn <lakshmikanthadn@gmail.com>
2018-11-05 18:57:18 +0530

    feat(config): adding the env based config files

---
+++ src/config/dev.ts
@@ -0,0 +1,6 @@
+const config: any = {};
+// Aplication config.
+config.APP = {
+  env: 'dev',
+};
+export default config;
---
+++ src/config/test.ts
@@ -0,0 +1,6 @@
+const config: any = {};
+// Aplication config.
+config.APP = {
+  env: 'test',
+};
+export default config;
